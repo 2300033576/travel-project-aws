0a79945ca2aa0bfc8cee3872fea0717d75d3c2ef
2300033576 <2300033576@kluniversity.in>
2025-09-22 15:19:25 +0530

    first commit

---
+++ travel_booking_frontend-master/src/NavbarComponent/Footer.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div>
+      <div class="container my-5">
+        <footer class="text-center text-lg-start text-color">
+          <div class="container-fluid p-4 pb-0">
+            <section class="">
+              <div class="row">
+                <div class="col-lg-4 col-md-6 mb-4 mb-md-0 text-center">
+                <p>
+                  Booking your next adventure is just a click away! Explore our wide range of tours and destinations and find the perfect journey for you. Let us take care of the details so you can focus on making memories. Start your unforgettable trip with us today!!!.
+                </p>
+                </div>
+              </div>
+            </section>
+
+            <hr class="mb-4" />
+
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+              
+              </p>
+            </section>
+
+            <hr class="mb-4" />
+          </div>
+
+          <div class="text-center">
+            © 2025 Copyright:
+            <a class="text-color-3" href="#">
+              auratravels.com
+            </a>
+          </div>
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
